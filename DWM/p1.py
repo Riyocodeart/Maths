66a4ee7df007587ea4bc6e8aa8dd06ea8c52f792
Riya Shrikant Naikdhure <riyanaikdhure855@gmail.com>
2025-10-04 09:32:33 +0530

    Delete DWM directory

--- DWM/p1.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("healthcare-dataset-stroke-data.csv")
-ages = df['age'].dropna()
-print("mean:",ages.mean())
-print("median",ages.median())
-print("mode:",ages.mode().tolist())
